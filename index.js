0e11e7b9c1321a9eaa46a964c4571a3c49fc7e22
rianma <mmg9403@gmail.com>
2021-04-01 20:00:11 +0800

    Print pid after node.js app started

--- index.js
+++ index.js
@@ -14,5 +14,6 @@ app.use((req, res) => {
 const port = process.env.PORT || 6000
 
 app.listen(port, () => {
+  console.log('Process pid: ' + process.pid)
   console.log('App listening on port ' + port)
 })
