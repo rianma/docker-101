755b3f32904f255b3b861dd7df62a207a4387976
rianma <mmg9403@gmail.com>
2021-05-08 16:44:33 +0800

    refactor: 重构为使用 koa 代替 express

--- index.js
+++ index.js
@@ -1,18 +1,53 @@
-const express = require('express')
-const morgan = require('morgan')
+const Koa = require('koa')
+const Router = require('@koa/router');
+const morgan = require('koa-morgan')
 
-const app = express()
+const app = new Koa()
+
+const router = new Router()
 
 app.use(morgan('combined'))
 
-app.use((req, res) => {
-  res.json({
-    headers: req.headers,
-    originalUrl: req.originalUrl,
-    body: req.body,
-    method: req.method
+/**
+ * @param {import('koa').Context} ctx
+ * @param {import('koa').Next} next
+ */
+app.use(async (ctx, next) => {
+  console.log('#1 before-api-request')
+  await next()
+  console.log('#2 after-api-response')
+
+  if (ctx.res.statusCode === 403) {
+    ctx.body = 'Override Original Response Body'
+  }
+
+  console.log('#3 respones-body-after-override', ctx.body)
+  console.log('#4 leave-pre-middleware')
+})
+
+/**
+ * @param {import('koa').Context} ctx
+ */
+router.get('/status/403', (ctx) => {
+  ctx.status = 403
+  ctx.body = 'Operation Is Not Allowed'
+})
+
+/**
+ * @param {import('koa').Context} ctx
+ * @param {import('koa').Next} next
+ */
+router.get('/echo', (ctx) => {
+  ctx.body = JSON.stringify({
+    headers: ctx.req.headers,
+    originalUrl: ctx.originalUrl,
+    query: ctx.query,
+    method: ctx.req.method
   })
 })
+
+app.use(router.routes())
+app.use(router.allowedMethods())
 
 const port = process.env.PORT || 6000
 
