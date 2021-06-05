1d232ef7fa7e9cb860ca43e303f4cd6e8976b732
rianma <mmg9403@gmail.com>
2021-06-05 18:48:39 +0800

    refactor: extract router.js from app/index.js

--- app/index.js
+++ app/index.js
@@ -1,11 +1,9 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
-const Router = require('@koa/router');
 const morgan = require('koa-morgan')
+const router = require('./router.js')
 
 const app = new Koa()
-
-const router = new Router()
 
 app.use(bodyParser())
 app.use(morgan('combined'))
@@ -26,26 +24,6 @@ app.use(async (ctx, next) => {
   console.log('#3 respones-body-after-override', ctx.body)
   console.log('#4 leave-pre-middleware')
 })
-
-/**@param {import('koa').Context} ctx */
-const forbiddenHandler = (ctx) => {
-	ctx.status = 403
-	ctx.body = 'Operation Is Not Allowed'
-}
-router.get('/status/403', forbiddenHandler)
-
-/**@param {import('koa').Context} ctx */
-const echoHandler = (ctx) => {
-	ctx.body = JSON.stringify({
-		headers: ctx.req.headers,
-		originalUrl: ctx.originalUrl,
-		query: ctx.query,
-		method: ctx.req.method,
-		body: ctx.request.body
-	})
-}
-router.get('/echo', echoHandler)
-router.post('/echo', echoHandler)
 
 app.use(router.routes())
 app.use(router.allowedMethods())
---
+++ app/router.js
@@ -0,0 +1,25 @@
+const Router = require('@koa/router')
+
+const router = new Router()
+
+/** @param {import('koa').Context} ctx */
+const forbiddenHandler = (ctx) => {
+  ctx.status = 403
+  ctx.body = 'Operation Is Not Allowed'
+}
+router.get('/status/403', forbiddenHandler)
+
+/** @param {import('koa').Context} ctx */
+const echoHandler = (ctx) => {
+  ctx.body = JSON.stringify({
+    headers: ctx.req.headers,
+    originalUrl: ctx.originalUrl,
+    query: ctx.query,
+    method: ctx.req.method,
+    body: ctx.request.body
+  })
+}
+router.get('/echo', echoHandler)
+router.post('/echo', echoHandler)
+
+module.exports = router
