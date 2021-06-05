01849eb6faec9e4cd49f813808690c3ad6ea5033
rianma <mmg9403@gmail.com>
2021-06-05 16:56:12 +0800

    refactor: separate koa app and server

---
+++ app/index.js
@@ -0,0 +1,52 @@
+const Koa = require('koa')
+const Router = require('@koa/router');
+const morgan = require('koa-morgan')
+
+const app = new Koa()
+
+const router = new Router()
+
+app.use(morgan('combined'))
+
+/**
+ * @param {import('koa').Context} ctx
+ * @param {import('koa').Next} next
+ */
+app.use(async (ctx, next) => {
+  console.log('#1 before-api-request')
+  await next()
+  console.log('#2 after-api-response')
+
+  if (ctx.res.statusCode === 403) {
+    ctx.body = 'Override Original Response Body'
+  }
+
+  console.log('#3 respones-body-after-override', ctx.body)
+  console.log('#4 leave-pre-middleware')
+})
+
+/**
+ * @param {import('koa').Context} ctx
+ */
+router.get('/status/403', (ctx) => {
+  ctx.status = 403
+  ctx.body = 'Operation Is Not Allowed'
+})
+
+/**
+ * @param {import('koa').Context} ctx
+ * @param {import('koa').Next} next
+ */
+router.get('/echo', (ctx) => {
+  ctx.body = JSON.stringify({
+    headers: ctx.req.headers,
+    originalUrl: ctx.originalUrl,
+    query: ctx.query,
+    method: ctx.req.method
+  })
+})
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+module.exports = app
