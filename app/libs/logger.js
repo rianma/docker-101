c4b16b6a92c84a126188355679c3d058989c46b8
rianma <mmg9403@gmail.com>
2021-08-08 18:49:06 +0800

    wip: setup logging middleware (using log4js)

---
+++ app/libs/logger.js
@@ -0,0 +1,16 @@
+const log4js = require('log4js')
+
+log4js.configure({
+  appenders: {
+    out: {
+      type: 'stdout',
+      layout: ''
+    }
+  }
+})
+
+const logger = log4js.getLogger()
+
+logger.level = 'debug'
+
+module.exports = logger
--- app/router.js
+++ app/router.js
@@ -7,4 +7,9 @@ const router = new Router()
 router.use('/echo', echoRouter.routes(), echoRouter.allowedMethods())
 router.use('/httpbin', httpbinRouter.routes(), httpbinRouter.allowedMethods())
 
+router.get('/errors', async (ctx) => {
+  await ctx.throw(403, 'Mock Forbidden Error', { code: 1001, expose: true })
+  // console.log('hey?')
+})
+
 module.exports = router
